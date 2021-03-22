7f3e9155079d0f940248171270bf9566a5ec2480
Jay Chun <jay.chun@tridge.com>
2021-03-22 17:39:24 +0900

    Remove outputs

--- src/components/Counter/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Counter = props => {
-    const { name } = props;
-    return React.createElement("p", null,
-        name,
-        " counter - tsx");
-};
-export default Counter;
--- src/components/Counter/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Counter = ({ name }) => {
-  return <p>{name} counter - jsx</p>
-}
-
-export default Counter;
